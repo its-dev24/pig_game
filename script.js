f354270b369aab7f3127b7c829dcd274492f0ac6
Gautham <gauthamskaniyattil@gmail.com>
2023-05-06 13:22:22 +0530

    intialised all elements and added partial functionality to roll dice button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+//selecting all elements
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
+let rollDiceBtn = document.querySelector('.btn--roll');
+
+//intializing
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//button functions
+
+//1.Roll dice button Function
+
+rollDiceBtn.addEventListener('click', function () {
+  if (diceEl.classList.contains('hidden')) {
+    diceEl.classList.remove('hidden');
+  }
+});
